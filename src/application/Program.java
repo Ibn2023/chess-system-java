c4663ee444c5a924c50d6558b5fe51c36104a896
Eduardo Abdulremane <eabdulremanee@gmail.com>
2025-07-26 22:59:55 +0100

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,19 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package application;
-
+import boardgame.Position;
 /**
  *
  * @author Eduardo
  */
 public class Program {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Ola");
+        
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+        
+
     }
     
 }
